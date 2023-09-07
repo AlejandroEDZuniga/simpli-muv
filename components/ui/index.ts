c3c599c94114c9b31424cc7d7d8bbf7e61fdb2bc
alejandroedzuniga <alejandroedzuniga@gmail.com>
2023-09-06 23:17:33 -0300

    Fix Barrel

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,4 +1,8 @@
 export { default as Card } from './Card';
+export { default as DetailSelectedProduct } from './DetailSelectedProduct';
+export { default as Footer } from './Footer';
+export { default as FormContact } from './FormContact';
 export { default as ItemList } from './ItemList';
-export * from './List';
 export { default as Navbar } from './Navbar';
+export { default as Select } from './Select';
+export { default as ThankYou } from './ThankYou';
